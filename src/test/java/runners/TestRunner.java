9e310c7603f58d803790d2c0d69b46a71e973230
Soumya Raj <rajsoumya135@gmail.com>
2025-08-18 12:29:48 +0530

    Update Xml file

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = "src/test/resources/features/UploadUC.feature",
+    features = "src/test/resources/features/PersonalDetails.feature",
     glue = "stepDefinitions",
     plugin = {
         "pretty",
